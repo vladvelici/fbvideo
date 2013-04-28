0843c3c6e20b91b89945ac02611434825c35b033
Vlad Velici <vlad.velici@gmail.com>
2013-04-28 05:54:46 +0300

    aws link

--- public/stream.js
+++ public/stream.js
@@ -5,7 +5,7 @@
       grabRate     : 70,
       canvasWidth  : 200,
       canvasHeight : 160,
-      socketSrv    : 'ws://fbvid.herokuapp.com:9000'
+      socketSrv    : 'ws://ec2-54-245-161-226.us-west-2.compute.amazonaws.com:9000'
     };
 
     var senderEl            = document.createElement('canvas');
